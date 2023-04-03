e5e2ffda1b14f6600d40f7203d15c458f32d4825
Celebi Ipek <58598855+celebipek@users.noreply.github.com>
2023-04-03 03:22:11 +0300

    Update app.js
    cleanCode

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-
-
-
 const API_URL = "https://api.exchangerate-api.com/v4/latest/";
 
 async function getExchangeRate(baseCurrency, targetCurrency) {
   try {
+
     const response = await fetch(`${API_URL}${baseCurrency}`);
     const data = await response.json();
     console.log(data)
-    const rate = data.rates[targetCurrency]
-    
-    
-    ;
-  
- 
-
+    const rate = data.rates[targetCurrency];
     return rate;
-
+    
   } catch (error) {
     console.log(error);
   }
 }
 
 
-
-// USD, EUR, GBP kurlarını al ve konsola yazdır
 getExchangeRate("USD", "TRY").then(rate => {
     document.getElementById("dollar").textContent = rate;
   
