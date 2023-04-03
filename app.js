681f8cf6eec683792814a947dd158ed38e97bd15
Celebi Ipek <58598855+celebipek@users.noreply.github.com>
2023-04-03 03:21:16 +0300

    Update app.js
    Temizlendi

--- app.js
+++ app.js
@@ -1,25 +1,8 @@
-// //l4b5Xa4j1dqphkWHttzxYAiXqNaXuqnq
-// const api = 'https://api.exchangeratesapi.io/v1/';
 
-// function params (paramsObj) {
-//      return new URLSearchParams ({
-//          access_key: 'l4b5Xa4j1dqphkWHttzxYAiXqNaXuqnq',
-//       ...paramsObj }); }
-
-// function getData(){
-//     fetch(api).then(res=>res.json())
-//     .then(data=>{
-//         console.log(data);
-//     })
-//     .catch((err) => console.warn(err));
-// }
-
-// getData(api);
 
 
 const API_URL = "https://api.exchangerate-api.com/v4/latest/";
 
-// Döviz kuru fonksiyonu
 async function getExchangeRate(baseCurrency, targetCurrency) {
   try {
     const response = await fetch(`${API_URL}${baseCurrency}`);
@@ -55,12 +38,6 @@ getExchangeRate("GBP", "TRY").then(rate => {
 
 });
 
-// function setData(USD, EURO, GBP) {
-//      const dollar = document.getElementById('dollar');
-//       const euro = document.getElementById('euro');
-//        const sterlin = document.getElementById('sterlin');
-//         dollar.textContent = USD.toString().slice (0, 4);
-//          euro.textContent = EURO.toString().slice(0, 4);
-//           sterlin.textContent = GBP; }
+
 
 
